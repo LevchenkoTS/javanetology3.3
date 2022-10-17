1035849d2d6fc1c416d93c9a3475435eebc2e719
Tatiana Levchenko <medvedovskaya.tatiana@gmail.com>
2022-10-18 00:30:11 +0300

    hw3.3

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        CreditPaymentService service = new CreditPaymentService();
+        double sum;
+        double creditTerm;
+        double percent;
+        double payment1 = service.calculate(1_000_000, 12, 9.99);
+        double payment2 = service.calculate(1_000_000, 24, 9.99);
+        double payment3 = service.calculate(1_000_000, 36, 9.99);
+
+
+        System.out.println(Math.round(payment1));
+        System.out.println(Math.round(payment2));
+        System.out.println(Math.round(payment3));
+    }
+
+}
